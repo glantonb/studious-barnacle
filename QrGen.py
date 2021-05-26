fb4fb286bf3919809614ab29e0b62217af19f6a7
Ben Glanton <Ben.Glanton@postcodelottery.co.uk>
2021-05-26 10:51:25 +0100

    Very, very basic generation of qr-code Adds Pillow (Python image generation) and

---
+++ QrGen.py
@@ -0,0 +1,20 @@
+"""
+This is spike-code for generating QR codes
+"""
+
+import qrcode
+
+# __SVG_FACTORY = qrcode.image.svg.SvgFragmentImage
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="black", back_color="white")
+
+img.save('./image.png')
